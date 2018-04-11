0ea7bca654a6d1a28e7d356c2c3a8c645c4c8f02
andbeck <andrew.beckerman@gmail.com>
2018-04-11 10:59:53 +0200

    habitat summarisation Rotation 1

--- hab_manipulation_data.R
+++ hab_manipulation_data.R
@@ -9,24 +9,63 @@ names(hab_master)
 hab_Litter <- hab_master %>% 
   gather(key = SampleLitter, value = LeafLitter,
          LeafLitter1:LeafLitter12) %>% 
-  select(Compartment, `Age of forest`, LoggingRotation, SampleLitter, LeafLitter)
+  select(Compartment, `Age of forest`, LoggingRotation, 
+         SampleLitter, LeafLitter)
 head(hab_Litter)
 
 hab_Canopy <- hab_master %>% 
   gather(key = SampleCanopy, value = CanopyClosure,
          CanopyClosure1:CanopyClosure12) %>% 
-  select(Compartment, `Age of forest`, LoggingRotation, SampleCanopy, CanopyClosure)
+  select(Compartment, `Age of forest`, LoggingRotation, 
+         SampleCanopy, CanopyClosure)
 
 hab_Herb <- hab_master %>% 
   gather(key = SampleHerb, value = HerbHeight,
        HerbHeight1:HerbHeight6) %>% 
-  select(Compartment, `Age of forest`, LoggingRotation, SampleHerb, HerbHeight)
+  select(Compartment, `Age of forest`, LoggingRotation, 
+         SampleHerb, HerbHeight)
 
-all_hab <- data.frame(
-  hab_Litter,
-  select(hab_Canopy, SampleCanopy, CanopyClosure),
-  select(hab_Herb, SampleHerb, HerbHeight)
-)
+dim(hab_Herb); dim(hab_Canopy); dim(hab_Litter)
 
-dim(all_hab)    
+Herb_means <- hab_Herb %>% 
+  group_by(Compartment, `Age of forest`, LoggingRotation) %>% 
+  summarise(herb_mean_height = mean(HerbHeight, na.rm = TRUE),
+            herb_sd_height = sd(HerbHeight, na.rm = TRUE),
+            herb_n_height = sum(!is.na(HerbHeight))) %>% 
+  filter(LoggingRotation == 1) %>% 
+  ungroup()
+head(Herb_means)
+
+Canopy_means <- hab_Canopy %>% 
+  group_by(Compartment, `Age of forest`, LoggingRotation) %>% 
+  summarise(canopy_mean_closure = mean(CanopyClosure, na.rm = TRUE),
+            canopy_sd_closure = sd(CanopyClosure, na.rm = TRUE),
+            canopy_n_closure = sum(!is.na(CanopyClosure))) %>% 
+  filter(LoggingRotation == 1) %>% 
+  ungroup() %>% 
+  select(canopy_mean_closure, canopy_sd_closure, canopy_n_closure)
+head(Canopy_means)
+
+Litter_means <- hab_Litter %>% 
+  group_by(Compartment, `Age of forest`, LoggingRotation) %>% 
+  summarise(litter_mean_depth = mean(LeafLitter, na.rm = TRUE),
+            litter_sd_depth = sd(LeafLitter, na.rm = TRUE),
+            litter_n_depth = sum(!is.na(LeafLitter))) %>% 
+  filter(LoggingRotation == 1) %>% 
+  ungroup() %>% 
+  select(litter_mean_depth, litter_sd_depth, litter_n_depth)
+head(Litter_means)
+
+hab_summary_rot1 <- data.frame(Herb_means, Litter_means, Canopy_means)
+hab_summary_rot1 <- select(hab_summary_rot1,
+                           Compartment,
+                           Age.of.forest,
+                           contains("_mean_"),
+                           contains("_sd_"),
+                           contains("_n_")
+                           )
+
+head(hab_summary_rot1)
+
+write_csv(hab_summary_rot1, path = "./SpeciesAdultData/hab_summary_rot1.csv")
 
