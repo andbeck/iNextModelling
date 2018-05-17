7412895534d3d79fe8baaedcf9ae06e747997cec
andbeck <andrew.beckerman@gmail.com>
2018-05-17 15:00:21 +0100

    create script to import gpx and find elevation

---
+++ Chapter2_Compartments/gpxConvert_getAlt.R
@@ -0,0 +1,21 @@
+library(plotKML)
+setwd('./DataSources/GPX')
+
+# identify gpx files that contain waypoints
+files <- list.files('./Chapter2_Compartments/DataSources/GPX')
+
+path <- './Chapter2_Compartments/DataSources/GPX/'
+
+readGPX('./Chapter2_Compartments/DataSources/GPX/Waypoints_08-Aug-16.gpx', tracks=FALSE, routes=FALSE)$waypoints[, c('name', 'lon', 'lat', 'time')]
+readGPX(paste(path,files[i], sep=""), tracks=FALSE, routes=FALSE)$waypoints[, c('name', 'lon', 'lat', 'time')]
+
+# combine all waypoints into a dataframe
+allwaypoints <- list()
+for (i in 1:length(files)) {
+  allwaypoints[[i]] <- readGPX(paste(path,files[i], sep=""), tracks=FALSE, routes=FALSE)$waypoints[, c('name', 'lon', 'lat', 'time', 'ele')]
+}
+
+allwaypoints <- do.call('rbind', allwaypoints)
+
+range(allwaypoints$ele)
+
