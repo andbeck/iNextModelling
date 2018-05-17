805fef15dc42ee6bf142e4a4fcbdb3c9ca1b12ea
andbeck <andrew.beckerman@gmail.com>
2018-05-17 18:17:15 +0100

    modify elevation estimate with correct data

--- Chapter2_Compartments/gpxConvert_getAlt.R
+++ Chapter2_Compartments/gpxConvert_getAlt.R
@@ -2,12 +2,12 @@ library(plotKML)
 setwd('./DataSources/GPX')
 
 # identify gpx files that contain waypoints
-files <- list.files('./Chapter2_Compartments/DataSources/GPX')
+files <- list.files('./Chapter2_Compartments/DataSources/GPX')[-1]
 
 path <- './Chapter2_Compartments/DataSources/GPX/'
 
-readGPX('./Chapter2_Compartments/DataSources/GPX/Waypoints_08-Aug-16.gpx', tracks=FALSE, routes=FALSE)$waypoints[, c('name', 'lon', 'lat', 'time')]
-readGPX(paste(path,files[i], sep=""), tracks=FALSE, routes=FALSE)$waypoints[, c('name', 'lon', 'lat', 'time')]
+# readGPX('./Chapter2_Compartments/DataSources/GPX/Waypoints_08-Aug-16.gpx', tracks=FALSE, routes=FALSE)$waypoints[, c('name', 'lon', 'lat', 'time')]
+# readGPX(paste(path,files[i], sep=""), tracks=FALSE, routes=FALSE)$waypoints[, c('name', 'lon', 'lat', 'time')]
 
 # combine all waypoints into a dataframe
 allwaypoints <- list()
@@ -16,6 +16,7 @@ for (i in 1:length(files)) {
 }
 
 allwaypoints <- do.call('rbind', allwaypoints)
+names(allwaypoints)
 
-range(allwaypoints$ele)
+range(na.omit(allwaypoints$ele))
 
