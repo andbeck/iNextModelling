6051d87583f0243054ef74d1fe0e06e9e5175c0e
andbeck <andrew.beckerman@gmail.com>
2018-07-15 07:20:37 +0100

    simplified iNEXT by compartment data prep

--- Chapter2_Compartments/AllCompartment_iNEXT.R
+++ Chapter2_Compartments/AllCompartment_iNEXT.R
@@ -10,23 +10,22 @@ distinct(compart, CompartmentName) # 26 compartments.
 # to apply iNEXT
 
 comp_age_df <- compart %>% 
-  mutate(ForestAge = ifelse(is.na(ForestAge), 200, ForestAge)) %>% 
-  select(ForestCategory, CompartmentName, ForestAge, Sp.01:Sp.27) %>% 
+  select(CompartmentName, Sp.01:Sp.27) %>% 
   # create data frame
-  gather(Species, Count, -ForestCategory, - CompartmentName, - ForestAge) %>%
+  gather(Species, Count, - CompartmentName) %>%
   # omit NA's
   na.omit() %>% 
   # convert Counts to numeric
   mutate(Count = as.numeric(Count)) %>% 
   # set up grouping
-  group_by(ForestCategory, CompartmentName, ForestAge, Species) %>% 
+  group_by(CompartmentName, Species) %>% 
   # get totals among transect by compartment
   summarise(Total = sum(Count, na.rm = TRUE)) %>% 
   # ungroup
   ungroup() %>% 
   # select the core data
   select(Species, CompartmentName, Total) %>% 
-  # spread out for iNEXT
+  # spread back out for iNEXT
   # compartment columns, species rows
   spread(CompartmentName, Total) %>% 
   # replace NA's with 0 counts
@@ -34,8 +33,10 @@ comp_age_df <- compart %>%
   # convert to data frame
   as.data.frame()
 
+# species by row - columns compartments
 tbl_df(comp_age_df) # 23/26 compartments.
 
+# iNEXT wants columns species, rownames compartments
 # organise rows and select the compartment columns
 rns <- comp_age_df$Species
 rownames(comp_age_df) <- rns
@@ -45,9 +46,9 @@ glimpse(comp_age_use)
 
 # Step 2 - which have 0 species....
 lost <- compart %>% 
-  select(ForestCategory, CompartmentName, ForestAge, Sp.01:Sp.27) %>% 
+  select(CompartmentName, Sp.01:Sp.27) %>% 
   # create data frame
-  gather(Species, Count, -ForestCategory, - CompartmentName, - ForestAge) %>% 
+  gather(Species, Count, - CompartmentName) %>% 
   # group at Compartment to see totals
   group_by(CompartmentName) %>% 
   mutate(Count = as.numeric(Count)) %>% 
@@ -62,9 +63,12 @@ lost # B18 BT7 UTT41 have 0 species.
 # Step A: iNEXT Asymp
 asymps <- iNEXT(comp_age_use)$AsyEst
 asymps
-write_csv(asymps, path = "~/Desktop/asymps.csv")
 
 # Step B: estimateDs
 coverage90 <- estimateD(comp_age_use, "abundance", 
                         base = 'coverage', level = 0.90)
-write_csv(asymps, path = "~/Desktop/coverage90.csv")
+coverage90
+
+# Step 4 write files
+write_csv(asymps, path = "~/Desktop/asymps.csv")
+write_csv(coverage90, path = "~/Desktop/coverage90.csv")
